edbfc311ef86c008c037425ba36a1da5aad03296
TaisiaKozharina <kozharina1306@gmail.com>
2022-10-30 15:20:11 +0200

    Added DB connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,14 @@
 import express, { Application, Request, Response} from 'express';
-import cosr from 'cors';
+import cors from 'cors';
+import mysql from 'mysql2';
 import "dotenv/config";
 
+
 const app:Application = express();
+app.use(cors());
 app.use(express.json()); //json parsing, strigifying
 
-
 //import routes
-
-
 
 //declare route paths
 app.get("/", (req:Request, res:Response) => {
@@ -19,14 +19,20 @@ console.log("Yay it works");
 
 
 const port:string|undefined = process.env.PORT;
-const start = async () => {
+
+const startServer = async () => {
     try{
+        const mysql = require('mysql2/promise');
+        //await mysql.createConnection(process.env.DBConnection);
+        await mysql.createConnection({host: process.env.DBhost, user:process.env.DBuser , password: process.env.DBpassword, database: process.env.DBname});
+        console.log("Connected to DB ✅");
+        //If connected to DB, start listening to port
         app.listen(port, () => console.log("Server listening on port ", port));
     }
     catch(err){
-        console.log("Error!");
+        console.log("Failed to connect to the DB ❌");
         console.log(err);
     }
 }
 
-start();
+startServer();
